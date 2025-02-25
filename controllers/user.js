d9077db5cc2c53a7be06229e448302ebaac9e0b9
GuiFernandes <GuiFernandesPro@gmail.com>
2025-02-25 15:36:11 +0100

    skeleton of the user fetching api functions and routes created

---
+++ controllers/user.js
@@ -0,0 +1,128 @@
+const User = require("../models/user.js");
+
+exports.fetchUser = async (req, res, next) => {
+	if (!req.isAuth) {
+		return res.status(403).json({ message: "Not Authorized" });
+	}
+
+	const userId = req.userId;
+
+	if (!userId) {
+		return res.status(400).json({ message: "No user found" });
+	}
+	try {
+		const user = await User.findById(userId);
+
+		if (!user) {
+			throw new Error("Failed to Fetch");
+		}
+
+		res.status(200).json({ user: user });
+	} catch (err) {
+		console.log(err);
+		return res.status(500).json({ message: "Failed to Fetch User" });
+	}
+};
+
+exports.uploadProfilePicture = async (req, res) => {
+	if (!req.isAuth) {
+		return res.status(403).json({ message: "Not Authorized" });
+	}
+
+	const userId = req.userId;
+
+	if (!userId) {
+		return res.status(400).json({ message: "No user found" });
+	}
+
+	const user = await User.findById(userId);
+
+	if (!user) {
+		return res.status(404).json({ message: "No user found" });
+	}
+
+	const profilePicture = req.body.file;
+
+	if (!profilePicture) {
+		return res.status(400).json({ message: "No file uploaded" });
+	}
+
+	user.profilePicture = profilePicture.path;
+
+	await user.save();
+
+	res
+		.status(200)
+		.json({ message: "Profile picture uploaded successfully", user });
+};
+
+exports.updateUserDetails = async (req, res) => {
+  if (!req.isAuth) {
+    return res.status(403).json({ message: "Not Authorized" });
+  }
+
+  const userId = req.userId;
+
+  if (!userId) {
+    return res.status(400).json({ message: "No user found" });
+  }
+
+  const user = await User.findById(userId);
+
+  if (!user) {
+    return res.status(404).json({ message: "No user found" });
+  }
+
+  const { name, email } = req.body;
+
+  if (!name || !email) {
+    return res.status(400).json({ message: "Invalid user data" });
+  }
+
+  user.name = name;
+  user.email = email;
+
+  await user.save();
+
+  res.status(200).json({ message: "User details updated successfully", user });
+};
+
+
+
+// TODO - Integrate bcrypt to hash the password before saving it
+// TODO - Add validation to check if the new password is the same as the old password with bcrypt
+exports.updatePassword = async (req, res) => {
+  if (!req.isAuth) {
+    return res.status(403).json({ message: "Not Authorized" });
+  }
+
+  const userId = req.userId;
+
+  if (!userId) {
+    return res.status(400).json({ message: "No user found" });
+  }
+
+  const user = await User.findById(userId);
+
+  if (!user) {
+    return res.status(404).json({ message: "No user found" });
+  }
+
+  const { currentPassword, newPassword } = req.body;
+
+  if (!currentPassword || !newPassword) {
+    return res.status(400).json({ message: "Invalid password data" });
+  }
+
+  const isMatch = await user.comparePasswords(currentPassword);
+
+  if (!isMatch) {
+    return res.status(400).json({ message: "Invalid password" });
+  }
+
+  user.password = newPassword;
+
+  await user.save();
+
+  res.status(200).json({ message: "Password updated successfully", user });
+};
--- routes/main.js
+++ routes/main.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const authControllers = require('../controllers/auth.js');
+const userControllers = require('../controllers/user.js');
 const habitControllers = require('../controllers/habit.js');
 
 const router = express.Router();
@@ -9,9 +10,15 @@ router.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+
+// =======================AUTH================================
+
 router.post('/signup', authControllers.signup);
 
 router.post('/login', authControllers.login);
+
+
+// =======================HABITS================================
 
 router.get('/fetch-habits', habitControllers.fetchHabits);
 
@@ -21,6 +28,15 @@ router.delete('/delete-habit/:habitId', habitControllers.deleteHabit);
 
 router.patch('/update-habit/:habitId', habitControllers.updateHabit);
 
-// router.post('/fetch-user', authControllers.fetchUser);
+
+// =======================USER================================
+
+router.get('/user-profile', userControllers.getUserProfile);
+
+router.post('/upload-profile-picture', userControllers.uploadProfilePicture);
+
+router.patch('/update-user-details', userControllers.updateUserDetails);
+
+router.patch('/update-password', userControllers.updatePassword);
 
 module.exports = router;
