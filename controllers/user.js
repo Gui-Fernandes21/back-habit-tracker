a751bf1a136d49199e412cfcb654f1653a358997
GuiFernandes <GuiFernandesPro@gmail.com>
2025-03-04 14:08:18 +0100

    refactor oneliner

--- controllers/user.js
+++ controllers/user.js
@@ -18,7 +18,7 @@ exports.getUserProfile = async (req, res, next) => {
 			"profilePicture",
 		]);
 
-		res.status(200).json({ user: user });
+		res.status(200).json({ user });
 	} catch (err) {
 		console.log(err);
 		return res.status(500).json({ message: "Failed to Fetch User" });
